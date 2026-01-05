bfd1244eb1e12174d30e5d7a5d540ade90839f30
Sushanth <littlecodly@gmail.com>
2026-01-05 21:48:48 +0530

    Challenge2 completed

--- Challenges/Challenge1.java
+++ Challenges/Challenge1.java
@@ -1,3 +1,4 @@
+//
 package Challenges;
 
 public class Challenge1 {
---
+++ Challenges/Challenge2.java
@@ -0,0 +1,9 @@
+//2.Create a new Project in Intelij Idea and output "subscribe" on the console.
+
+package Challenges;
+
+public class Challenge2 {
+  public static void main(String[] args) {
+        System.out.println("subscribe");
+    }
+}
