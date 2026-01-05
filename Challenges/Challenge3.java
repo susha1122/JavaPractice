ecad90d18b96d9560a0cc987b5f2c8ba7b3122d3
Sushanth <littlecodly@gmail.com>
2026-01-05 21:51:09 +0530

    Challenge3 completed

---
+++ Challenges/Challenge3.java
@@ -0,0 +1,32 @@
+//3.Show the following patterns:
+// 1. Right Half Pyramid
+// 2. Reverse Right Half Pyramid
+// 3. Left Half Pyramid
+package Challenges;
+
+public class Challenge3 {
+  public static void main(String[] args) {
+        System.out.println("Printing the Pattern\n");
+        System.out.println("Right Half Pyramid\n");
+        System.out.println("* * * * *");
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*\n");
+
+
+        System.out.println("Reverse Right Half Pyramid\n");
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        System.out.println("* * * * *\n");
+
+        System.out.println("Left Half Pyramid\n");
+        System.out.println("        *");
+        System.out.println("      * *");
+        System.out.println("    * * *");
+        System.out.println("  * * * *");
+        System.out.println("* * * * *");
+    }
+}
