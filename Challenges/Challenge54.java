cbc2d8086a73148525e2f82ec3f53820e35abe23
Sushanth <littlecodly@gmail.com>
2026-01-06 12:24:12 +0530

    Challenge54

---
+++ Challenges/Challenge54.java
@@ -0,0 +1,21 @@
+//Create a program to find if the given number is even or odd
+package Challenges;
+
+import java.util.Scanner;
+
+public class Challenge54 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Finding the given number is Odd or Even\n");
+
+    System.out.print("Enter any number: ");
+    int num = input.nextInt();
+
+    String oddEven = num % 2 == 0 ? "Even" : "Odd";
+    
+    System.out.println("The given number is " + oddEven);
+
+    input.close();
+  }
+}
