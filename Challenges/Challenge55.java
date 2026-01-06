bc4e4afc22aeaad7e1706f04bff140f3f8e25202
Sushanth <littlecodly@gmail.com>
2026-01-06 13:01:45 +0530

    Challenge55 completed

---
+++ Challenges/Challenge55.java
@@ -0,0 +1,22 @@
+//Create a program to calculate the absolute value of a given integer.
+
+
+package Challenges;
+
+import java.util.Scanner;
+
+public class Challenge55 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Calculate the absolute of the Intege\nr");
+
+    System.out.print("Enter any number: ");
+    int num = input.nextInt();
+
+    int result = num >= 0 ? num : -num;
+    System.out.println("The absolute Value of the Given Number is " + result);
+
+    input.close();
+  }
+}
