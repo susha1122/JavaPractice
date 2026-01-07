291a79ee01ed64e1adc120ac4482238d12fbe3c3
Sushanth <littlecodly@gmail.com>
2026-01-07 21:41:20 +0530

    Challenge4 completed

---
+++ Challenges/Challenge4.java
@@ -0,0 +1,33 @@
+/*
+4.Show the following patterns using single print statement:
+  1. Right Half Pyramid
+  2. Reverse Right Half Pyramid
+  3. Left Half Pyramid
+*/
+package challenges;
+
+public class Challenge4 {
+  public static void main(String[] args) {
+        System.out.println("Printing the Pattern\n");
+        System.out.println("Right Half Pyramid\n");
+        System.out.println("* * * * *\n" +
+                "* * * *\n" +
+                "* * *\n" +
+                "* *\n" +
+                "*\n" );
+
+        System.out.println("Reverse Right Half Pyramid\n");
+        System.out.println("*\n" +
+                "* *\n" +
+                "* * *\n" +
+                "* * * *\n" +
+                "* * * * *\n");
+
+        System.out.println("Left Half Pyramid\n");
+        System.out.println("        *\n" +
+                "      * *\n" +
+                "    * * *\n" +
+                "  * * * *\n" +
+                "* * * * *");
+    }
+}
