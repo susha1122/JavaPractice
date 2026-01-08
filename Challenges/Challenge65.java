69c3ff9b976809de028057bc5a52940fee25a2b6
Sushanth <littlecodly@gmail.com>
2026-01-08 10:25:16 +0530

    Challenge65 completed

--- Challenges/Challenge65.java
+++ Challenges/Challenge65.java
@@ -1,9 +1,26 @@
-//reate a program using break to read inputs from the user in a loop and break the loop if a specific keyword (like "exit") is entered.
+//create a program using break to read inputs from the user in a loop and break the loop if a specific keyword (like "exit") is entered.
 
 package challenges;
 
+import java.util.Scanner;
+
 public class Challenge65 {
   public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Enter Exist to exist from the Loop");
+
+    String keyWord = "Exist";
     
+    while (true) {
+      System.out.print("Type anything (Except Exist): ");
+      String userInput = input.next();
+      if (userInput.equals(keyWord)) {
+        break;
+      }
+    }
+    
+
+    input.close();
   }
 }
