b6138695c6fdb830434aa13ab9a023b977383b5c
Sushanth <littlecodly@gmail.com>
2026-01-08 11:28:13 +0530

    Challenge69 completed

--- Challenges/Challenge69.java
+++ Challenges/Challenge69.java
@@ -10,7 +10,29 @@ public class Challenge69 {
 
     System.out.println("Palindrome\n");
 
-    
+    System.out.print("Enter anything: ");
+    String userInput = input.next();
+
+    int left = 0;
+    int right = userInput.length() - 1;
+
+    boolean isPalindromes = isPalindrome(userInput, left, right);
+    if (isPalindromes) {
+      System.out.println("The given string is Palindrome");
+    } else {
+      System.out.println("The given string is not Palindrome");
+    }
+
     input.close();
+  }
+
+  public static boolean isPalindrome (String userInput, int left, int right) {
+    if (left >= right) {
+      return true;
+    }
+    if (userInput.charAt(left) != userInput.charAt(right)){
+      return false;
+    }
+    return isPalindrome(userInput, left+1, right-1);
   }
 }
