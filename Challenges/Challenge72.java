bb5e6a774432a57ccfa6d0717ad03ada7f319f60
Sushanth <littlecodly@gmail.com>
2026-01-09 20:10:17 +0530

    Challenge72

--- Challenges/Challenge72.java
+++ Challenges/Challenge72.java
@@ -4,10 +4,21 @@ package challenges;
 
 public class Challenge72 {
   public static void main(String[] args) {
-    
+    DiceRoll diceRoll = new DiceRoll();
+    System.out.println(diceRoll);
+  }
+  static class DiceRoll {
+    int dice;
+
+    DiceRoll() {
+      dice = (int)(Math.random() * 6) + 1;
+    }
+
+    @Override
+    public String toString() {
+      return "Dice Rolled " + dice;
+    }
   }
 }
 
-static class diceRoll {
-  
-}
+
