d386cc1cb73ada0e1f8c5aa51ec0ba71e2ad0a3f
Sushanth <littlecodly@gmail.com>
2026-01-10 10:09:17 +0530

    Challenge9 completed

---
+++ Challenges/Challenge9.java
@@ -0,0 +1,22 @@
+//9. Create a program to calculate product of two floating points numbers.
+
+package challenges;
+
+import java.util.Scanner;
+
+public class Challenge9 {
+  public static void main(String[] args) {
+        System.out.println("Product of Two Floating Point");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter floating point number 1: ");
+        float num1 = input.nextFloat();
+        System.out.print("Enter floating point number 2: ");
+        float num2 = input.nextFloat();
+
+        //product
+        float product = num1 * num2;
+
+        //print
+        System.out.printf("The product of two floating point number is, %f", product);
+    }
+}
