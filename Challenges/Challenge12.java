7dcb47a78881ac08fd2bfc20b7486a596d735ffa
Sushanth <littlecodly@gmail.com>
2026-01-12 21:10:21 +0530

    Challenge13 completed

---
+++ Challenges/Challenge12.java
@@ -0,0 +1,28 @@
+//12. Create a program to calculate simple interest. Simple Interest = (P x T x R)/100
+
+package challenges;
+
+import java.util.Scanner;
+
+public class Challenge12 {
+  public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("=== Simple Interest Calculator ===");
+
+        System.out.print("Enter the principal amount: ");
+        double principal = scanner.nextDouble();
+
+        System.out.print("Enter the time period (in years): ");
+        double time = scanner.nextDouble();
+
+        System.out.print("Enter the annual interest rate (in %): ");
+        double rate = scanner.nextDouble();
+
+        // Calculate Simple Interest
+        double simpleInterest = (principal * time * rate) / 100;
+
+        System.out.printf("Simple Interest = %.2f%n", simpleInterest);
+
+        scanner.close();
+    }
+}
---
+++ Challenges/Challenge13.java
@@ -0,0 +1,29 @@
+//13.Create a program to calculate Compound interest. Compound Interest = P(1 + R/100)t
+
+package challenges;
+
+import java.util.Scanner;
+
+public class Challenge13 {
+  public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("=== Compound Interest Calculator ===");
+
+        System.out.print("Enter the principal amount: ");
+        double principal = input.nextDouble();
+
+        System.out.print("Enter the time period (in years): ");
+        double time = input.nextDouble();
+
+        System.out.print("Enter the annual interest rate (in %): ");
+        double rate = input.nextDouble();
+
+        //formula
+        double ci = principal * (1 + rate/100) * time;
+
+        //final print statement
+        System.out.printf("The compound interest is %.2f%n",ci);
+
+        input.close();
+    }
+}
