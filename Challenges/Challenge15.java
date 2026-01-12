7cc37e7c12ad418892aac54e18eb9497d5509987
Sushanth <littlecodly@gmail.com>
2026-01-12 21:17:10 +0530

    Challenge15 completed

---
+++ Challenges/Challenge15.java
@@ -0,0 +1,27 @@
+//15.Create a program that determines if a number is positive, negative, or zero.
+
+package challenges;
+
+import java.util.Scanner;
+
+public class Challenge15 {
+  public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Determining the Number is +, - or 0");
+
+        //input
+        System.out.print("Enter the Number: ");
+        int num = input.nextInt();
+
+        //logic
+        if (num > 0) {
+            System.out.println("The number is positive");
+        } else if (num < 0 ) {
+            System.out.println("The number is Negative");
+        } else {
+            System.out.println("the number is zero");
+        }
+
+        input.close();
+    }
+}
