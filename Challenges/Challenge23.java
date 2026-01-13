30ba8464e2e040641b823c895f25e43546f46efc
Sushanth <littlecodly@gmail.com>
2026-01-13 23:02:39 +0530

    Challenge23 completed

---
+++ Challenges/Challenge23.java
@@ -0,0 +1,24 @@
+//23. Create a program that shows bitwise XOR of two numbers.
+
+package challenges;
+
+import java.util.Scanner;
+
+public class Challenge23 {
+  public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Bitwise XOR Operator\n");
+
+        //input
+        System.out.print("Enter 1st Number: ");
+        int a = input.nextInt();
+        System.out.print("Enter 2nd Number: ");
+        int b = input.nextInt();
+
+        //logic
+        int result = a ^ b;
+
+        //final print statement
+        System.out.println("The Result is: " + result);
+    }
+}
